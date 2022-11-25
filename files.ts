791f23ac89e426ba000538a30ce07ba550412630
Vyacheslav Zamaraev <zamaraev@gmail.com>
2022-11-25 03:19:08 +0000

    write file

--- files.ts
+++ files.ts
@@ -1,4 +1,13 @@
+// export DENO_INSTALL="/home/codespace/.deno"
+// export PATH="$DENO_INSTALL/bin:$PATH"
+// Run '/home/codespace/.deno/bin/deno --help' to get started
+
+
+
 const decoder = new TextDecoder('utf-8')
 const data = await Deno.readfile('./LICENSE')
 console.log(decoder.decode(data));
 
+const encoder = new TextEncoder();
+const newText = 'New text to write to file';
+await DelayNode.writeFile('./newtextfile, encoder');
